ec5898362720c46c6a361d8d8a4bb14cdc6d4b18
Tuanpahe163791 <122149575+Tuanpahe@users.noreply.github.com>
2023-02-05 22:40:11 +0700

    Update SignUp.java

--- HappyProgrammingProject/src/java/Controller/SignUp.java
+++ HappyProgrammingProject/src/java/Controller/SignUp.java
@@ -99,7 +99,11 @@ public class SignUp extends HttpServlet {
         }
     }
 
-    
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
     @Override
     public String getServletInfo() {
         return "Short description";
