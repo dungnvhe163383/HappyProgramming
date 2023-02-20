7f4ec7926da6dddd89ca2063e6c2ffc6ff245a4f
dungnvhe163383 <dungnvhe163383@fpt.edu.vn>
2023-02-20 13:47:06 +0700

    add in DAO get Mentor, skill, status of a request

--- HappyProgrammingProject/src/java/DAO/DAO.java
+++ HappyProgrammingProject/src/java/DAO/DAO.java
@@ -212,7 +212,7 @@ public class DAO extends DBContext {
         }
     }
     
-    public Request viewRequestDetail(int id){
+     public Request viewRequestDetail(int id){
         Request request = new Request();
         query = "select *from Request where id=?";
         try {
@@ -223,5 +223,48 @@ public class DAO extends DBContext {
         } catch (Exception e) {
         }
         return request;
+    }
+    
+    public Status getStatusOfRequest(int id){
+        Status status = new Status();
+        query = "select s.Status from request r, status s where r.statusID=s.ID and r.id=?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            status=new Status(rs.getInt(1), rs.getString(2));
+        } catch (Exception e) {
+        }
+        return status;
+    }
+    
+    public List<Skill> getSkillOfRequest(int id){
+        List<Skill> list=new ArrayList<>();
+        query="select s.name from skill s,requestskill rs,request r where s.id=rs.skillID and r.id=rs.requestID and r.id=?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            while (rs.next()){
+                list.add(new Skill(rs.getInt(1), rs.getString(2)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+    
+    public List<Mentor> getMentorOfRequest(int id){
+        List<Mentor> list=new ArrayList<>();
+        query="select m.name from mentor m,mentorRequest ms,request r where m.id=ms.mentorID and r.id=ms.requestID and r.id=?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            while (rs.next()){
+                list.add(new Mentor(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getDate(6), rs.getString(7), rs.getString(8), rs.getString(9), rs.getString(10), rs.getString(11), rs.getString(12), rs.getInt(13)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
     }
 }
