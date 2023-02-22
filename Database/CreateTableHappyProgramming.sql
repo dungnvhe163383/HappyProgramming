649a3736379ac3745fab85ebcba806276e93536c
dungnvhe163383 <dungnvhe163383@fpt.edu.vn>
2023-02-22 23:27:43 +0700

    change database

---
+++ Database/CreateTableHappyProgramming.sql
@@ -0,0 +1,156 @@
+﻿--drop database HappyProgramming
+--create database HappyProgramming
+use HappyProgramming
+---------------------------------------------- Table roles -------------------------------
+
+create table roles (
+	id int identity(1, 1) primary key,
+	[name] nvarchar(50)
+)
+
+---------------------------------------------- Table account -------------------------------
+
+create table account (
+	id int identity(1, 1) primary key,
+	username varchar(50),
+	[password] varchar(50),
+	roleID int foreign key references Roles(ID)
+)
+
+---------------------------------------------- Table Mentee -------------------------------
+
+
+create table mentee (
+	id int foreign key references account(id) primary key,
+	[address] nvarchar(250),
+	email nvarchar(250),
+	[name] nvarchar(250) ,
+	phone nvarchar(250),
+	birthday Date ,
+	sex nvarchar(250) ,
+	avatar nvarchar(250)
+)
+
+
+---------------------------------------------image-------------------------------------------
+create table [Image](
+	id int identity(1, 1) primary key,
+	accountid int foreign key references account(id),
+	[image] nvarchar(250),
+)
+
+---------------------------------------------- Table Mentor -------------------------------
+-- 1 bảng tách họ, tên đệm, tên
+create table mentor(
+	id int foreign key references Account(ID) primary key,
+	[address] nvarchar(250),
+	email nvarchar(250) unique,
+	[name] nvarchar(250) ,
+	phone nvarchar(250) ,
+	birthday Date ,
+	sex nvarchar(250) ,
+	introduce nvarchar(250),
+	profession nvarchar(250),
+	professionIntroduce nvarchar(250),
+	achievement nvarchar(250),
+	avatar nvarchar(250),
+	costHire int
+)
+---------------------------------------------- Table Status -------------------------------
+
+create table status(
+	id int identity(1, 1) primary key,
+	[Status] nvarchar(250)
+)
+
+
+---------------------------------------------- Table Request(các request do Mentee đưa ra) -------------------------------
+create table request (
+	id int identity(1, 1) primary key,
+	title nvarchar(250),
+	content nvarchar(250),	
+	deadline date,
+	menteeID int foreign key references mentee(id),
+)
+
+
+create table requestStatus(
+	requestID int foreign key references request(id),
+	statusID int foreign key references status(id),
+	primary key (requestID, statusID)
+)
+
+
+
+create table Feedback(
+	id int identity(1, 1) primary key,
+	requestID int foreign key references request(id),
+	mentorID int foreign key references mentor(id),
+	rate int,
+	commentDetail nvarchar(250),
+)
+
+---------------------------------------------- Table Skill -------------------------------
+
+
+create table skill(
+	id int identity(1, 1) primary key,
+	[name] nvarchar(250),
+)
+
+create table requestSkill(
+	skillID int foreign key references skill(id),
+	requestID int foreign key references request(id),
+	primary key(skillID, requestID)
+)
+
+create table mentorRequest(
+	requestID int foreign key references request(id),
+	mentorID int foreign key references mentor(id),
+	primary key(requestID, mentorID)
+)
+
+
+create table historyRequest(
+	id int identity(1, 1) primary key,
+	title nvarchar(250),
+	content nvarchar(250),
+	deadline date,
+	accountID int foreign key references Account(id),
+	statusID int foreign key references Status(id)
+)
+
+
+create table hire(
+-- bảng mentee gửi yêu cầu thuê mentor 
+	mentorID int foreign key references mentor(id),
+	menteeID int foreign key references mentee(id),
+	primary key (mentorID,menteeID),
+	content nvarchar(250),
+	statusID int foreign key references Status(id) --(thuê, đã từng thuê, không thuê)
+)
+
+---------------------------------------------- Table MentorSkill ( mentor có nhiều skill) -------------------------------
+
+create table mentorSkill(
+	skillID int foreign key references skill(id),
+	mentorID int foreign key references mentor(id),
+	primary key(skillID, mentorID)
+)
+
+
+create table [following](
+	mentorID int foreign key references mentor(id),
+	menteeID int foreign key references mentee(id),
+	primary key (mentorID,menteeID),
+	statusID int foreign key references Status(id) --(follow, unfollow)
+)
+
+create table answerRequest(
+	content nvarchar(250),
+	requestID int foreign key references request(id),
+	mentorid int foreign key references mentor(id),
+	primary key (requestid,mentorid)
+)
+
+
--- Database/insertData.sql
+++ Database/insertData.sql
@@ -9,17 +9,8 @@ insert into Account values ('mentor2', 'mentor', 2);
 insert into Account values ('admin1', 'admin', 3);
 
 
-insert into Mentee values  (1,'Nghe an','mentee@email1', 'Nguyen Duy Manh', '0123456789','11-03-2002', 'M', 'avatar3.jpg')
-insert into Mentee values  (2,'Ha Noi', 'mentee@email2', 'Nguyen Vu Dung', '0987654321','11-04-2002', 'F', 'avatar4.jpg')
-
-
-insert into Status values ('Accept');
-insert into Status values ('Not yet');
-insert into Status values ('Done');
-insert into Status values ('Reject');
-insert into Status values ('Hired');
-insert into Status values ('not hire');
-
+insert into Mentee values  (1,'Nghe an','manhndhe161791@fpt.edu.vn', 'Nguyen Duy Manh', '0123456789','11-03-2002', 'M', 'avatar3.jpg')
+insert into Mentee values  (2,'Ha Noi', 'longnhhe164030@fpt.edu.vn', 'Nguyen Vu Dung', '0987654321','11-04-2002', 'F', 'avatar4.jpg')
 
 
 insert into Mentor values (3,'Ho Chi Minh', 'okanhchaphet7@gmail.com', 'Pham Anh Tuan', '0887654321','11-05-2002', 'M', 
@@ -28,18 +19,65 @@ insert into Mentor values (4,'Ha Noi', 'nguyenvudung96@gmail.com', 'Nguyen Hoang
 'toi la mot nguoi co trach nhiem va muon mang den cho ban nhieu kien thức','Developer','toi code phan mem và xay dung ke hoach kinh phat trien phan mem','tot nghiep dai hoc fpt', 'avatar2.jpg',400);
 
 
-insert into Request values ('Teach me java', 'I wanna learn java', 1, '11-02-2023', 2);
-insert into Request values ('Teach me mockito', 'I wanna learn mockito', 2, '10-02-2023', 2);
-insert into Request values ('Teach me C', 'I wanna learn C', 2, '12-02-2023', 2);
-insert into Request values ('Teach me C++', 'I wanna learn C++', 1, '11-02-2023', 2);
-insert into Request values ('Teach me C#', 'I wanna learn C#', 1, '10-02-2023', 2);
-insert into Request values ('Teach me Python', 'I wanna learn Python', 2, '11-02-2023', 2);
-insert into Request values ('Teach me R', 'I wanna learn R', 2, '10-02-2023', 2);
-insert into Request values ('Teach me PhoneGap', 'I wanna learn PhoneGap', 1, '11-02-2023', 2);
+insert into Status values ('must do');--kiểu request ( bắt phải làm hay có thể từ chối)
+insert into Status values ('invite');
+insert into Status values ('hire');--mentee thuê mentor
+insert into Status values ('Hired');
+insert into Status values ('unhire');
+insert into Status values ('follow');--mentee follow mentor
+insert into Status values ('unfollow');
+insert into Status values ('answered');--request
+insert into Status values ('accept');--chấp nhận thuê hay ko
+insert into Status values ('reject');
+insert into Status values ('not yet');
 
-insert into rate values (5,5)
-insert into rate values (6,4)
-insert into rate values (7,4)
+
+insert into Request values ('Teach me java', 'I wanna learn java','02-25-2023', 2);
+insert into Request values ('Teach me mockito', 'I wanna learn mockito','02-26-2023', 1);
+insert into Request values ('Teach me C', 'I wanna learn C','02-25-2023', 2);
+insert into Request values ('Teach me C++', 'I wanna learn C++','02-25-2023', 1);
+insert into Request values ('Teach me C#', 'I wanna learn C#','02-26-2023', 1);
+insert into Request values ('Teach me Python', 'I wanna learn Python', '02-26-2023', 2);
+insert into Request values ('Teach me R', 'I wanna learn R','02-25-2023', 1);
+insert into Request values ('Teach me PhoneGap', 'I wanna learn PhoneGap','02-25-2023', 2);
+
+
+insert into requestStatus values (9,11)
+insert into requestStatus values (9,2)
+insert into requestStatus values (10,8)
+insert into requestStatus values (10,1)
+insert into requestStatus values (11,11)
+insert into requestStatus values (11,2)
+insert into requestStatus values (12,8)
+insert into requestStatus values (12,1)
+insert into requestStatus values (5,11)
+insert into requestStatus values (5,2)
+insert into requestStatus values (6,8)
+insert into requestStatus values (6,1)
+insert into requestStatus values (7,11)
+insert into requestStatus values (7,2)
+insert into requestStatus values (8,8)
+insert into requestStatus values (8,1)
+
+insert mentorRequest values (5,3)
+insert mentorRequest values (6,3)
+insert mentorRequest values (7,3)
+insert mentorRequest values (8,3)
+insert mentorRequest values (9,3)
+insert mentorRequest values (10,3)
+insert mentorRequest values (5,4)
+insert mentorRequest values (6,4)
+insert mentorRequest values (7,4)
+insert mentorRequest values (11,4)
+insert mentorRequest values (12,4)
+
+
+
+insert into feedback values (10,3,5,'good1')
+insert into feedback values (6,4,5,'good2')
+insert into feedback values (12,4,5,'good3')
+insert into feedback values (6,3,5,'good4')
+insert into feedback values (8,3,5,'good5')
 
 
 
@@ -48,31 +86,29 @@ insert into Skill values('Python')
 insert into Skill values('C++')
 insert into Skill values('C#')
 
-insert into historyMenteeRequest values('want to know javax', 'I wanna learn javax', 1, '11-02-2023')
-insert into historyMenteeRequest values('want to know css', 'I wanna learn css', 1, '11-02-2023')
-insert into historyMenteeRequest values('want to know spring', 'I wanna learn spring', 2, '11-02-2023')
+
+insert into requestSkill values(1,5)
+insert into requestSkill values(2,12)
+insert into requestSkill values(3,6)
+insert into requestSkill values(4,6)
+insert into requestSkill values(4,7)
+insert into requestSkill values(3,8)
+insert into requestSkill values(2,9)
+insert into requestSkill values(1,10)
+insert into requestSkill values(3,11)
+
+insert into historyRequest values('want to know javax', 'I wanna learn javax','02-13-2023',1,11)
+insert into historyRequest values('want to know css', 'I wanna learn css','02-25-2023',2,8)
+insert into historyRequest values('want to know spring', 'I wanna learn spring','02-12-2023',3,11)
+insert into historyRequest values('want to know boostrap', 'I wanna learn boostrap','02-25-2023',4,8)
+insert into historyRequest values('want to know R', 'I wanna learn R','02-20-2023',4,11)
 
 
-insert into historyMentorRequest values('want to know spring', 'I wanna learn spring', 3, '11-02-2023')
-insert into historyMentorRequest values('want to know springs', 'I wanna learn springs', 3, '11-02-2023')
-insert into historyMentorRequest values('want to learn html', 'I wanna learn html', 4, '11-02-2023')
 
 
-insert into hire values(3,1,'can I hire you',5)
-insert into hire values(4,1,'can I hire you1',6)
+insert into hire values(3,1,'can I hire you',3)
+insert into hire values(4,1,'can I hire you1',4)
 insert into hire values(3,2,'can I hire you2',5)
-
-insert into inviteRequest values(1,3,4)
-insert into inviteRequest values(2,4,1)
-insert into inviteRequest values(4,3,4)
-
-insert into hireRequest values(3,3,2)
-insert into hireRequest values(5,4,3)
-insert into hireRequest values(6,3,3)
-insert into hireRequest values(7,4,3)
-insert into hireRequest values(8,3,2)
-
-
 
 insert into mentorSkill values(1,3)
 insert into mentorSkill values(2,3)
@@ -81,42 +117,11 @@ insert into mentorSkill values(1,4)
 insert into mentorSkill values(4,4)
 insert into mentorSkill values(2,4)
 
-insert into comment values ('good',5)
-insert into comment values ('good1',6)
-insert into comment values ('good2',7)
+insert into following values (4,2,7)
 
 
-
-insert into requestSkill values(1,1)
-insert into requestSkill values(2,2)
-insert into requestSkill values(3,1)
-insert into requestSkill values(4,3)
-insert into requestSkill values(4,4)
-insert into requestSkill values(3,5)
-insert into requestSkill values(2,6)
-insert into requestSkill values(1,7)
-insert into requestSkill values(3,8)
-
-
-insert into mentorRequest values (1,3)
-insert into mentorRequest values (1,4)
-insert into mentorRequest values (2,3)
-insert into mentorRequest values (3,4)
-insert into mentorRequest values (4,3)
-insert into mentorRequest values (5,3)
-insert into mentorRequest values (6,4)
-insert into mentorRequest values (7,3)
-insert into mentorRequest values (8,4)
-
-insert into following values (4,1)
-insert into following values (4,2)
-insert into following values (3,1)
-
-insert into historyHire values (4,2,'can I hire you please')
-
-insert into historyFollow values (3,2)
-
-insert into answerRequest values ('go to link to know detail',5,3)
-insert into answerRequest values ('go to link1 to know detail',5,4)
-insert into answerRequest values ('go to link2 to know detail',6,4)
-insert into answerRequest values ('go to link3 to know detail',7,4)
+insert into answerRequest values ('join A link to know ',10,3)
+insert into answerRequest values ('join B link to know ',6,4)
+insert into answerRequest values ('join C link to know ',12,4)
+insert into answerRequest values ('join D link to know ',6,3)
+insert into answerRequest values ('join E link to know ',8,3)
